617d54308774c5dcfe53cb89427cb1bc5890e930
RoxBot <me@roxedus.dev>
2021-05-28 06:27:47 +0000

    Added New Release

--- publish/clean.sh
+++ publish/clean.sh
@@ -14,7 +14,7 @@ else
     branch="develop"
 fi
 
-aptly repo remove -config="repo/${branch}.aptly.conf" "${package}" '$Version (<< 4.0.1330)'
+aptly repo remove -config="repo/${branch}.aptly.conf" "${package}" '$Version (<< 4.0.1390)'
 aptly publish update -config="repo/${branch}.aptly.conf" -batch -passphrase="${INPUT_GPG_PASSPHRASE}" "${distro}" filesystem:public:${branch}
 aptly db cleanup -config="repo/${branch}.aptly.conf"
 aptly publish update -config="repo/${branch}.aptly.conf" -batch -passphrase="${INPUT_GPG_PASSPHRASE}" "${distro}" filesystem:public:${branch}
